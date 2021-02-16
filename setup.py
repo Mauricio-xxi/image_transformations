3b269e0e036ba93a6fa9c7182207bb286360dff5
Álvaro Camacho <mauricio-xxi@hotmail.com>
2021-02-16 23:38:50 +0100

    delete not needed code

--- setup.py
+++ setup.py
@@ -2,11 +2,6 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
-
 VERSION = '0.0.1'
 DESCRIPTION = 'A basic image transformations using numpy and opencv'
 LONG_DESCRIPTION = 'A package that allows to modify images and vectorize an image using numpy and opencv.'
